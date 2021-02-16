54c8f1781ae4109d65477aef6c5da4d982719ae9
Kelpie <ariel.p20@mhs.istts.ac.id>
2021-02-16 17:34:44 +0800

    finishing dictionary

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let length = 0;
 const ALPHA  = 0;
 //dictionary buat cutnya
+/**Describe cut quality of the diamond. Quality in increasing order Fair, Good, Very Good, Premium, Ideal */
 let dictForCut = {
    "Fair" : 0.2, 
    "Good" : 0.4, 
@@ -9,6 +10,9 @@ let dictForCut = {
    "Ideal":1
 }
 //dictionary buat color
+/**
+ * Color of the diamond, with D being the best and J the worst
+ */
 let dictForColor = {
    "D" : 1,
    "E" : 6/7,
@@ -19,8 +23,20 @@ let dictForColor = {
    "J" : 1/7,
 }
 //dictionary buat clarity 
+/*How obvious inclusions are within the diamond:(in order from best to worst, FL = flawless, I3= level 3 inclusions)
+FL,IF, VVS1, VVS2, VS1, VS2, SI1, SI2, I1, I2, I3 */
 let dictForClarity = {
-
+   "FL" : 11/11,
+   "IF" : 10/11, 
+   "VVS1" : 9/11, 
+   "VVS2": 8/11, 
+   "VS1": 7/11, 
+   "VS2" : 6/11, 
+   "SI1" : 5/11,
+   "SI2" : 4/11,
+   "I1" : 3/11,
+   "I2" : 2/11,
+   "I3" : 1/11, 
 }
 
 //getting the json into a loadable data
